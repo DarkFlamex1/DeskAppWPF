484956adbc4e6ad50bbf6e14312096fd70b66cc6
Vikram Peddinti <vikram.peddinti@gmail.com>
2026-03-14 18:03:20 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace DeskAppWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Make sure that the text box contains a name.
+        /// Validate that the name entered in the text box doesn't already exist.
+        /// Add the name to the list box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                // Add the name to the list box
+                lstNames.Items.Add(txtName.Text);
+
+                // Clear the text box
+                txtName.Clear();
+            }
+            else
+            {
+                MessageBox.Show($"Please enter a valid name that is not already in the list.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}

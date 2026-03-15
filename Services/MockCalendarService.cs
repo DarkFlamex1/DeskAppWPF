782365603404aa1921bd6f1d8b4b25b33597c2b1
Vikram Peddinti <vikram.peddinti@gmail.com>
2026-03-15 15:58:35 -0700

    Add the mock calendar service for testing locally.

---
+++ Services/MockCalendarService.cs
@@ -0,0 +1,40 @@
+﻿using DeskAppWPF.Models;
+using Ical.Net.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskAppWPF.Services
+{
+    internal class MockCalendarService : ICalendarService
+    {
+        public MockCalendarService()
+        {
+            // allow for setting up of specific settings if needed for testing
+        }
+
+        public async Task<IEnumerable<UpcomingEvent>> GetUpcomingEventsAsync(string icsUrl)
+        {
+            if (string.IsNullOrWhiteSpace(icsUrl))
+            {
+                return Array.Empty<UpcomingEvent>();
+            }
+
+
+            // return a mocked meeting that's always starting NOW!
+            var result = new List<UpcomingEvent>();
+            result.Add(new UpcomingEvent
+            {
+                Uid = "12345",
+                Summary = "MEETING NOW: MISS IT AND YOUR FIRED",
+                StartTime = DateTime.Now.ToLocalTime(),
+                EndTime = DateTime.Now.AddMinutes(30).ToLocalTime()
+            });
+
+            return result.AsEnumerable();
+        }
+    }
+}

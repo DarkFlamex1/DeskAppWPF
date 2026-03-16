5d08e17f3c80e525e6d5e0c5cc7d5bb7e3f4a0c7
Vikram Peddinti <vikram.peddinti@gmail.com>
2026-03-15 17:51:28 -0700

    Converted the calendar view to a calendar overview of events. - Cleanly shows the next 5 days in the calendar. - Split out the CalendarEvent's into their own view given that they have their own ViewModel.

---
+++ Views/CalendarEventView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace DeskAppWPF.Views
+{
+    public partial class CalendarEventView : UserControl
+    {
+        public CalendarEventView()
+        {
+            InitializeComponent();
+        }
+    }
+}
